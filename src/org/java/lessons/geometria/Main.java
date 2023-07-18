ecc13299cdbe594b8c5733145655a217c966c5e1
renat <guitarra123primera@gmail.com>
2023-07-18 15:12:11 +0200

    Finished the Rettangolo class!

--- src/org/java/lessons/geometria/Main.java
+++ src/org/java/lessons/geometria/Main.java
@@ -1,5 +1,7 @@
 package org.java.lessons.geometria;
-
+//Creare una classe Main contenente il metodo main(), nel quale chiedete all’utente di inserire, da console,
+// i valori di base e di altezza con cui istanziare un nuovo Rettangolo. Dopo averlo istanziato, stampate a video
+// il perimetro e l’area.
 public class Main {
     public static void main(String[] args) {
 
--- src/org/java/lessons/geometria/Rettangolo.java
+++ src/org/java/lessons/geometria/Rettangolo.java
@@ -1,4 +1,40 @@
 package org.java.lessons.geometria;
-
+//una classe Rettangolo con due attributi interi: base e altezza. Aggiungere un opportuno costruttore con parametri.
+// Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il
+// perimetro del rettangolo.
 public class Rettangolo {
+    //CAMPI base e altezza
+    int base;
+    int height;
+
+
+
+    //COSTRUTTORI con parametri
+    Rettangolo (int base, int height){
+        this.base = base;
+        this.height = height;
+    }
+
+    //METODI = Calcola area e Calcola perimetro
+    int getArea(){
+        //calcoliamo l'area del rettangolo
+            //dichiariamo variabili per base e altezza
+            int baseArea = base;
+            int heightArea = height;
+
+            //calcolo l'area
+            int areaRet= baseArea * heightArea;
+        return areaRet;
+    }
+    int getPerimetro (){
+        int basePerimetro=base;
+        int heightPerimetro = height;
+
+        //calcolo il perimetro
+        int perimetroRet = (2 * basePerimetro) + (2 * heightPerimetro);
+        return perimetroRet;
+    }
+
+
+
 }
